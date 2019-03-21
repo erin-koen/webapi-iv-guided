85695db4696b74d922f922451360bea8f7c5b0b8
erin-koen <erin.koen@gmail.com>
2019-03-21 13:54:08 -0400

    ENV taken out

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
 const server = require('./api/server.js');
 
 
 //make the port dynamic - process brings in env object methods that allow postgres server to choose. If you don't have it hooked up, it'll go to 4000
 
 const port = process.env.PORT || 4000;
-const greeting = process.env.GREETING
+const greeting = process.env.GREETING || 'no message';
 
 server.listen(port, () => {
   console.log(`\n*** ${greeting} Server Running on http://localhost:${port} ***\n`);
